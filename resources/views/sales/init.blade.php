311cdbeae78aae68cdd89e8be7318371a412de1e
bethojunior <hgomescostajr@gmail.com>
2020-11-01 16:06:50 -0300

    tudo OK

--- resources/views/sales/init.blade.php
+++ resources/views/sales/init.blade.php
@@ -1,4 +1,55 @@
 
+<div class="row col l12 s12 m12 filter">
+    <div class="col l12 s12 m12">
+        <p class="title-card">Filtros</p>
+    </div>
+    <div class="input-field col l2 s12 m12">
+        <select>
+            <option value="1">Vendas</option>
+            <option value="2">Locação</option>
+            <option value="3">Lançamentos</option>
+        </select>
+        <label>Tipo</label>
+    </div>
+    <div class="input-field col l2 s12 m12">
+        <select>
+            <option value="1">Apartamento</option>
+            <option value="2">Casa</option>
+            <option value="3">Condominio</option>
+        </select>
+        <label>Modelo</label>
+    </div>
+    <div class="input-field col l2 s12 m12">
+        <select>
+            <option value="1">Juazeiro do Norte</option>
+            <option value="2">Barbalha</option>
+            <option value="3">Crato</option>
+            <option value="3">Milagres</option>
+        </select>
+        <label>Cidade</label>
+    </div>
+    <div class="input-field col l2 s12 m12">
+        <select>
+            <option value="0">Escolha a cidade</option>
+            <option value="1">Pedrinhas</option>
+            <option value="2">Centro</option>
+            <option value="3">Bairro de Fátima</option>
+        </select>
+        <label>Bairro</label>
+    </div>
+    <div class="input-field col l2 s12 m12">
+        <select>
+            <option value="1">Até R$100.000,00</option>
+            <option value="2">R$100.000,00 à R$300.000,00</option>
+            <option value="3">R$300.000,00 à R$500.000,00</option>
+        </select>
+        <label>Valor</label>
+    </div>
+    <div class="input-field col l2 s12 m12">
+        <button class=" col l12 s12 m12 btn btn-info">Aplicar</button>
+    </div>
+</div>
+
 
 <div class="row">
     <div class="col s12 l3">
@@ -9,29 +60,29 @@
                         <li>
                             <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/584/i0X49iwxlL696q1c9702_5845f86ff3379957.jpg"> <!-- random image -->
                             <div class="caption center-align">
-{{--                                <h3>Consulte</h3>--}}
-{{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
                             </div>
                         </li>
                         <li>
                             <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/584/i0qaLNbw4_5845b06fbf94c574.jpg"> <!-- random image -->
                             <div class="caption left-align">
-{{--                                <h3>Consulte</h3>--}}
-{{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
                             </div>
                         </li>
                         <li>
                             <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/584/i0qaLNbw4_5845b06fc5035f21.jpg"> <!-- random image -->
                             <div class="caption right-align">
-{{--                                <h3>Consulte</h3>--}}
-{{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
                             </div>
                         </li>
                         <li>
                             <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/584/i0X49iwxlL696q1c9702_5845f86ff1d245c0.jpg"> <!-- random image -->
                             <div class="caption center-align">
-{{--                                <h3>Consulte</h3>--}}
-{{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
                             </div>
                         </li>
                     </ul>
@@ -178,6 +229,190 @@
             <div class="card-content">
                 <span class="card-title">Condominio <label>Juazeiro do norte - Ce</label></span>
                 <label>R$300.000,00</label>
+                <p>
+                    Novo empreedimento, Cidade de Barbalha
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col s12 l3">
+        <div class="card">
+            <div class="card-image">
+                <div class="slider">
+                    <ul class="slides">
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/110/i59E1PO_11059d53be85eb63.jpg"> <!-- random image -->
+                            <div class="caption center-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/110/i59E1PO_11059d53be510a6f.jpg"> <!-- random image -->
+                            <div class="caption left-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/110/i59E1PO_11059d53be619808.jpg"> <!-- random image -->
+                            <div class="caption right-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/110/i59E1PO_11059d53be3bc927.jpg"> <!-- random image -->
+                            <div class="caption center-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <a class="btn-floating halfway-fab waves-effect waves-light red modal-sales"><i class="fa fa-eye" aria-hidden="true"></i></a>
+            </div>
+            <div class="card-content">
+                <span class="card-title">Condominio <label>Juazeiro do norte - Ce</label></span>
+                <label>R$300.000,00</label>
+                <p>
+                    Novo empreedimento, Cidade de Barbalha
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 l3">
+        <div class="card">
+            <div class="card-image">
+                <div class="slider">
+                    <ul class="slides">
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/696/iaew9_6965be424e6dc841.jpg"> <!-- random image -->
+                            <div class="caption center-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/696/iaew9_6965be424e386573.jpg"> <!-- random image -->
+                            <div class="caption left-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/696/iaew9_6965be424e12ca62.jpg"> <!-- random image -->
+                            <div class="caption right-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/696/iaew9_6965be424dd0b8ed.jpg"> <!-- random image -->
+                            <div class="caption center-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <a class="btn-floating halfway-fab waves-effect waves-light red modal-sales"><i class="fa fa-eye" aria-hidden="true"></i></a>
+            </div>
+            <div class="card-content">
+                <span class="card-title">Apartamento </span>
+                <label for="">R$550.000,00</label>
+                <p>
+                    Novo empreedimento, Cidade de Barbalha
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 l3">
+        <div class="card">
+            <div class="card-image">
+                <div class="slider">
+                    <ul class="slides">
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/1016/ibaaQx6K1322V5ggw_10165f9703eeeef91.jpg"> <!-- random image -->
+                            <div class="caption center-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/1016/ibaaQx6K1322V5ggw_10165f9703f0dc3e4.jpg"> <!-- random image -->
+                            <div class="caption left-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/1016/ibaaQx6K1322V5ggw_10165f9703f2b3250.jpg"> <!-- random image -->
+                            <div class="caption right-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/1016/ibaaQx6K1322V5ggw_10165f9703f49a389.jpg"> <!-- random image -->
+                            <div class="caption center-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <a class="btn-floating halfway-fab waves-effect waves-light red modal-sales"><i class="fa fa-eye" aria-hidden="true"></i></a>
+            </div>
+            <div class="card-content">
+                <span class="card-title">Sitio <label>Area rural do Crato - Ce</label></span>
+                <label>R$ 750.000,00 </label>
+                <p>
+                    Novo empreedimento, Cidade de Barbalha
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 l3">
+        <div class="card">
+            <div class="card-image">
+                <div class="slider">
+                    <ul class="slides">
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/584/i0X49iwxlL696q1c9702_5845f86ff3379957.jpg"> <!-- random image -->
+                            <div class="caption center-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/584/i0qaLNbw4_5845b06fbf94c574.jpg"> <!-- random image -->
+                            <div class="caption left-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/584/i0qaLNbw4_5845b06fc5035f21.jpg"> <!-- random image -->
+                            <div class="caption right-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                        <li>
+                            <img src="https://cdn.vistahost.com.br/canuto6551/vista.imobi/fotos/584/i0X49iwxlL696q1c9702_5845f86ff1d245c0.jpg"> <!-- random image -->
+                            <div class="caption center-align">
+                                {{--                                <h3>Consulte</h3>--}}
+                                {{--                                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>--}}
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <a class="btn-floating halfway-fab waves-effect waves-light red modal-sales"><i class="fa fa-eye" aria-hidden="true"></i></a>
+            </div>
+            <div class="card-content">
+                <span class="card-title">Consulte</span>
                 <p>
                     Novo empreedimento, Cidade de Barbalha
                 </p>
