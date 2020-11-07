08feb72df13bb988c1e53359eb1da99c19b80114
bethojunior <hgomescostajr@gmail.com>
2020-11-07 20:27:19 -0300

    listing

--- resources/views/properties/index.blade.php
+++ resources/views/properties/index.blade.php
@@ -12,14 +12,23 @@
     <div class="row col-lg-12">
         @foreach($properties as $property)
             <div class="row col-lg-12 card">
-                {{ $property->title }}
+                <p>Titulo : {{ $property->title }}</p>
+                <p>Sub Titulo : {{ $property->sub_title }}</p>
+                <p>Valor: {{ $property->value }}</p>
+                <p>Valor Adicional: {{ $property->value_additional }}</p>
+                <p>Criado: {{ Carbon\Carbon::parse($property->created_at )->format('d/m/Y') }}</p>
+                <p>Cidade : {{ $property->city['0']['name'] }}</p>
+                <p>Bairro : {{ $property->neighborhood['0']['name'] }}</p>
+                <p>Tipo : {{ $property->acquisition['0']['name'] }}</p>
+                <p>Modelo : {{ $property->model['0']['name'] }}</p>
+                <p>Endereço : {{ $property->address }}</p>
                 <div class="accordion" id="accordionExample">
                     @foreach($property->images as $images)
                         <div class="card">
                             <div class="card-header" id="headingThree{{$images->id}}">
                                 <h2 class="mb-0">
-                                    <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree{{$images->id}}" aria-expanded="false" aria-controls="collapseThree{{$images->id}}">
-                                        Collapsible Group Item #3
+                                    <button class="btn btn-link btn-block text-left " type="button" data-toggle="collapse" data-target="#collapseThree{{$images->id}}" aria-expanded="false" aria-controls="collapseThree{{$images->id}}">
+                                        Imagem {{ $images->id }}
                                     </button>
                                 </h2>
                             </div>
